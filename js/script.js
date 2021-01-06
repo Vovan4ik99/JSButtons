85015653ac3482d5851303b056a987f20dd7dba8
Volodymyr Sukhina <sukhina.vova@gmail.com>
2021-01-06 17:05:41 +0100

    poprawiony scrypt JS

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 'use strict';
 
-function hide() {
-    document.getElementById('btn').style.visibility = 'hidden';
-}
-function hide1() {
-    document.getElementById('btn1').style.visibility = 'hidden';
-}
-function hide2() {
-    document.getElementById('btn2').style.visibility = 'hidden';
-}
-function hide3() {
-    document.getElementById('btn3').style.visibility = 'hidden';
-}
-function hide4() {
-    document.getElementById('btn4').style.visibility = 'hidden';
-}
+function hide (ElemId) {
+    ElemId.onclick = function () {
+        if (ElemId.style.opacity = 1) {
+            ElemId.style.opacity = 0; 
+        }   
+    }
+}   
 
- 
+hide(document.getElementById('btn'));
+hide(document.getElementById('btn1'));
+hide(document.getElementById('btn2'));
+hide(document.getElementById('btn3'));
+hide(document.getElementById('btn4'));
