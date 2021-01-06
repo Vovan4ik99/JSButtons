346d01a64c5b4c13379ea8ec4969c753804d85a8
Volodymyr Sukhina <sukhina.vova@gmail.com>
2021-01-06 01:13:03 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function hide() {
+    document.getElementById('btn').style.visibility = 'hidden';
+}
+function hide1() {
+    document.getElementById('btn1').style.visibility = 'hidden';
+}
+function hide2() {
+    document.getElementById('btn2').style.visibility = 'hidden';
+}
+function hide3() {
+    document.getElementById('btn3').style.visibility = 'hidden';
+}
+function hide4() {
+    document.getElementById('btn4').style.visibility = 'hidden';
+}
+
+ 
